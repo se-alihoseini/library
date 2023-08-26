f67f0575092e68281720c78e857a8ccc599d95e8
se-alihoseini <se.alihoseini@gmail.com>
2023-08-26 16:17:24 +0330

    add user manager and user creation form

--- user/forms.py
+++ user/forms.py
@@ -5,23 +5,14 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 
 class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = '__all__'
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password2 != password1 :
-            raise ValidationError('password dont match')
-        return password2
-
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
         return user
--- user/manager.py
+++ user/manager.py
@@ -19,7 +19,8 @@ class MyUserManager(BaseUserManager):
 
         user = self.model(email=self.normalize_email(email), phone_number=phone_number,
                           first_name=first_name, last_name=last_name)
-        user.set_password(password)
+        if password is not None:
+            user.set_password(password)
         user.save(using=self._db)
         return user
 
