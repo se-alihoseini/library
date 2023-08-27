84926b5440690c3b6d45149b9a66fa40e08db352
se-alihoseini <se.alihoseini@gmail.com>
2023-08-27 07:23:10 +0330

    edit book serializer and model

--- book/models.py
+++ book/models.py
@@ -5,11 +5,12 @@ from django.utils.text import slugify
 class Book(models.Model):
     title = models.CharField(max_length=30)
     slug = models.CharField(unique=True, blank=True, null=True)
-    author = models.ManyToManyField('Author')
-    genre = models.ForeignKey('Genre', related_name='genre_book', on_delete=models.CharField)
+    description = models.TextField()
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='author_book')
+    genre = models.ManyToManyField('Genre', related_name='genre_books')
     publication_date = models.DateField()
     isbn = models.CharField(max_length=30)
-    price = models.BigIntegerField()
+    price = models.PositiveBigIntegerField()
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
@@ -23,6 +24,7 @@ class Book(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=30)
     slug = models.CharField(unique=True, null=True, blank=True)
+    city = models.ForeignKey('City', on_delete=models.CASCADE, related_name='city_authors')
 
     def __str__(self):
         return self.name
@@ -36,6 +38,21 @@ class Genre(models.Model):
     name = models.CharField(max_length=30)
     slug = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Genre, self).save(*args, **kwargs)
+
 
 class City(models.Model):
     name = models.CharField(max_length=30)
+    slug = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(City, self).save(*args, **kwargs)
--- book/serializer.py
+++ book/serializer.py
@@ -3,6 +3,12 @@ from book.models import Book
 from rest_framework import serializers
 
 
+class BookCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('title', 'description', 'author', 'genre', 'publication_date', 'isbn', 'price')
+
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
