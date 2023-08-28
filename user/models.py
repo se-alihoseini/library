a5a82a4a77c0e19ec4d941802cf3cf9e3aa0bb9a
se-alihoseini <se.alihoseini@gmail.com>
2023-08-28 23:22:56 +0330

    add Transaction model

--- user/models.py
+++ user/models.py
@@ -76,3 +76,18 @@ class OtpCode(models.Model):
             return True
         else:
             return False
+
+
+class Transaction(models.Model):
+    STATUS_CHOICES = [
+        ('su', 'successful'),
+        ('un', 'unsuccessful')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_transaction')
+    transaction_code = models.CharField(max_length=20)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    cost = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}-{self.created_at}'
