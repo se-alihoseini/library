ba424fce7ae5d77825c99256c881f2c92b90ca76
se-alihoseini <se.alihoseini@gmail.com>
2023-08-31 08:29:55 +0330

    edit Book model and create Reservation model

--- book/models.py
+++ book/models.py
@@ -1,5 +1,8 @@
+from user.models import User
+
 from django.db import models
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
 
 
 class Book(models.Model):
@@ -12,13 +15,32 @@ class Book(models.Model):
     isbn = models.CharField(max_length=30)
     price = models.PositiveBigIntegerField()
     is_deleted = models.BooleanField(default=False)
+    availability = models.IntegerField()
+    in_reserve = models.PositiveIntegerField()
+    version = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
+        if self.price < 0:
+            raise ValidationError('price cannot be negative')
         super(Book, self).save(*args, **kwargs)
+
+
+class Reservation(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='books_reserved')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reserved')
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField()
+    cost = models.PositiveBigIntegerField()
+
+    def save(self, *args, **kwargs):
+        if self.cost < 0:
+            raise ValidationError('cost cannot be negative')
+        if (self.end_date - self.start_date).days > 14:
+            raise ValidationError('reserve days cannot more than 14 days')
 
 
 class Author(models.Model):
