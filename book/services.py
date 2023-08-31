97c11e677e246994af90f2b6b1f0003988e7165b
se-alihoseini <se.alihoseini@gmail.com>
2023-08-31 08:31:47 +0330

    add book_reserves and normal_user_reserves and vip_user_reserves and book_reserved_list and discount_calculator in book_service

--- book/services.py
+++ book/services.py
@@ -22,18 +22,41 @@ def delete_book(pk):
     return repositories.delete_book(pk)
 
 
-def book_reserves(self):
-    pass
+def book_reserves(days, user, book):
+    if user.subscription_status:
+        data = vip_user_reserves(days, user, book)
+    else:
+        data = normal_user_reserves(days, user, book)
+    return data
 
 
-def book_reserved_list(self):
-    pass
+def normal_user_reserves(days, user, book):
+    if days > 7:
+        raise ValueError('days cannot more than 7 days')
+    else:
+        cost = days*1000
+        total_cost = discount_calculator(user=user, cost=cost)
+        reserve = repositories.create_reservation(days, user, book, total_cost)
+        return reserve
 
 
-def reserves_cost_calculator(self):
-    pass
+def vip_user_reserves(days, user, book):
+    if days > 14:
+        raise ValueError('days cannot more than 7 days')
+    else:
+        cost = 0
+        reserve = repositories.create_reservation(days, user, book, cost)
+        return reserve
 
 
-def discount_calculator(self):
-    pass
+def book_reserved_list(user):
+    return repositories.get_book_reserved_list()
 
+
+def discount_calculator(user, cost):
+    reservation_count = repositories.get_3_month_user_books_count(user=user)
+    if reservation_count > 3:
+        cost = cost*0.7
+        if repositories.get_2_month_user_books_cost(user=user) > 300000:
+            cost = 0
+    return cost
